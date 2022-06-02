015e8b4481dffee46314e7e5f4fc529a8bfa0c09
TheSunshade <82448383+thesunshade@users.noreply.github.com>
2022-06-02 10:14:54 +0530

    buttons

--- index.js
+++ index.js
@@ -7,75 +7,92 @@ homeButton.addEventListener("click", () => {
   document.location.search = "";
 });
 
+const form = document.getElementById("form");
+const citation = document.getElementById("citation");
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  // buildSutta(citation.value);
+  document.location.search = "?" + citation.value.replace(/\s/g, "");
+});
+
+citation.value = document.location.search.replace("?", "").replace(/%20/g, "").replace(/\s/g, "");
+
 function buildSutta(slug) {
   slug = slug.toLowerCase();
   let html = `<div class="button-area"><button id="hide-pali" class="hide-button">Toggle Pali</button></div>`;
 
-  const contentResponse = fetch(`https://suttacentral.net/api/bilarasuttas/${slug}/sujato?lang=en`)
-    .then(response => response.json())
-    .catch(error => {
-      suttaArea.innerHTML = `Sorry, "${decodeURIComponent(slug)}" is not a valid sutta citation.
-      <br><br>
-      Note: <br>
-      Citations cannot contain spaces.<br>
-      Chapter and sutta number should be separated by a period.<br>
-      Only dn, mn, sn, and an are valid books.<br>
-      Suttas that are part of a series require that you enter the exact series.`;
-    });
+  const contentResponse = fetch(`https://suttacentral.net/api/bilarasuttas/${slug}/sujato?lang=en`).then(response =>
+    response.json()
+  );
 
   const suttaplex = fetch(`https://suttacentral.net/api/suttas/${slug}/sujato?lang=en&siteLanguage=en`).then(response =>
     response.json()
   );
 
-  Promise.all([contentResponse, suttaplex]).then(responses => {
-    const [contentResponse, suttaplex] = responses;
-    const { html_text, translation_text, root_text, keys_order } = contentResponse;
-    console.log("next");
-    console.log(suttaplex.root_text.next);
+  Promise.all([contentResponse, suttaplex])
+    .then(responses => {
+      const [contentResponse, suttaplex] = responses;
+      const { html_text, translation_text, root_text, keys_order } = contentResponse;
+      console.log(responses);
+      keys_order.forEach(segment => {
+        if (translation_text[segment] === undefined) {
+          translation_text[segment] = "";
+        }
+        let [openHtml, closeHtml] = html_text[segment].split(/{}/);
+        // openHtml = openHtml.replace(/^<span class='verse-line'>/, "<br><span class='verse-line'>");
+        html += `${openHtml}<span class="segment" id ="${segment}"><span class="pli-lang" lang="pi">${root_text[segment]}</span><span class="eng-lang" lang="en">${translation_text[segment]}</span></span>${closeHtml}\n\n`;
+      });
+      const scLink = `<p class="sc-link"><a href="https://suttacentral.net/${slug}/en/sujato">On SuttaCentral.net</a></p>`;
+      suttaArea.innerHTML = scLink + html;
+      const pageTile = document.querySelector("h1");
+      document.title = pageTile.textContent;
 
-    keys_order.forEach(segment => {
-      if (translation_text[segment] === undefined) {
-        translation_text[segment] = "";
-      }
-      let [openHtml, closeHtml] = html_text[segment].split(/{}/);
-      // openHtml = openHtml.replace(/^<span class='verse-line'>/, "<br><span class='verse-line'>");
-      html += `${openHtml}<span class="pli-lang" lang="pi">${root_text[segment]}</span><span class="eng-lang" lang="en">${translation_text[segment]}</span>${closeHtml}\n\n`;
+      toggleThePali();
+      next.innerHTML = suttaplex.root_text.next.name
+        ? `<a href="?${suttaplex.root_text.next.uid}">${suttaplex.root_text.next.name}<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="body_1" width="15" height="11">
+
+      <g transform="matrix(0.021484375 0 0 0.021484375 2 -0)">
+        <g>
+              <path d="M202.1 450C 196.03278 449.9987 190.56381 446.34256 188.24348 440.73654C 185.92316 435.13055 187.20845 428.67883 191.5 424.39L191.5 424.39L365.79 250.1L191.5 75.81C 185.81535 69.92433 185.89662 60.568687 191.68266 54.782654C 197.46869 48.996624 206.82434 48.91536 212.71 54.6L212.71 54.6L397.61 239.5C 403.4657 245.3575 403.4657 254.8525 397.61 260.71L397.61 260.71L212.70999 445.61C 209.89557 448.4226 206.07895 450.0018 202.1 450z" stroke="none" fill="#000000" fill-rule="nonzero" />
+        </g>
+      </g>
+      </svg></a>`
+        : "";
+      previous.innerHTML = suttaplex.root_text.previous.name
+        ? `<a href="?${suttaplex.root_text.previous.uid}"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="body_1" width="15" height="11">
+
+      <g transform="matrix(0.021484375 0 0 0.021484375 2 -0)">
+        <g>
+              <path d="M353 450C 349.02106 450.0018 345.20444 448.4226 342.39 445.61L342.39 445.61L157.5 260.71C 151.64429 254.8525 151.64429 245.3575 157.5 239.5L157.5 239.5L342.39 54.6C 346.1788 50.809414 351.70206 49.328068 356.8792 50.713974C 362.05634 52.099876 366.10086 56.14248 367.4892 61.318974C 368.87753 66.49547 367.3988 72.01941 363.61002 75.81L363.61002 75.81L189.32 250.1L363.61 424.39C 367.90283 428.6801 369.18747 435.13425 366.8646 440.74118C 364.5417 446.34808 359.06903 450.00275 353 450z" stroke="none" fill="#000000" fill-rule="nonzero" />
+        </g>
+      </g>
+      </svg>${suttaplex.root_text.previous.name}</a>`
+        : "";
+    })
+    .catch(error => {
+      suttaArea.innerHTML = `Sorry, "${decodeURIComponent(slug)}" is not a valid sutta citation.
+    <br><br>
+    Note: <br>
+    Suttas that are part of a series require that you enter the exact series. For example, <code>an1.1</code> will not work, but <code>an1.1-10</code> will.<br>
+    Citations cannot contain spaces.<br>
+    Chapter and sutta number should be separated by a period.<br>
+    Only dn, mn, sn, and an are valid books.`;
     });
-    const scLink = `<p class="sc-link"><a href="https://suttacentral.net/${slug}/en/sujato">On SuttaCentral.net</a></p>`;
-    suttaArea.innerHTML = scLink + html;
-    const pageTile = document.querySelector("h1");
-    document.title = pageTile.textContent;
-
-    toggleThePali();
-    next.innerHTML = suttaplex.root_text.next.name
-      ? `<a href="?${suttaplex.root_text.next.uid}">${suttaplex.root_text.next.name}🠖</a>`
-      : "";
-    previous.innerHTML = suttaplex.root_text.previous.name
-      ? `<a href="?${suttaplex.root_text.previous.uid}">🠔 ${suttaplex.root_text.previous.name}</a>`
-      : "";
-  });
 }
 
-// initialize
+// initialize the whole app
 if (document.location.search) {
-  buildSutta(document.location.search.replace("?", ""));
+  buildSutta(document.location.search.replace("?", "").replace(/\s/g, "").replace(/%20/g, ""));
 } else {
   suttaArea.innerHTML = `<div class="instructions">
   <p>Citations must exactly match those found on SuttaCentral.net. No spaces. Separate chapter and sutta with a period. The following books work:</p>
   <ul>
-      <li>DN</li>
-      <li>MN</li>
-      <li>SN</li>
-      <li>AN</li>
-      <li>Kp</li>
+      <li>DN</li>      <li>MN</li>      <li>SN</li>      <li>AN</li>      <li>Kp</li>
       <li>Dhp (must have exact range)</li>
       <li>Ud</li>
       <li>Iti (1–112)</li>
-      <li>Snp</li>
-      <li>Thag</li>
-      <li>Thig</li>
+      <li>Snp</li>      <li>Thag</li>      <li>Thig</li>
   </ul>
-
   <p>Suttas that are part of a series require that you enter the exact series.</p>
 </div>`;
 }
@@ -102,65 +119,3 @@ function toggleThePali() {
     }
   });
 }
-
-function parseSlug(slug) {
-  const slugParts = slug.match(/^([a-z]+)(\d*)\.*(\d*)/);
-  const book = slugParts[1];
-  const firstNum = slugParts[2];
-
-  if (book === "dn" || book === "mn") {
-    return `${book}/${slug}`;
-  } else if (book === "sn" || book === "an") {
-    return `${book}/${book}${firstNum}/${slug}`;
-  } else if (book === "kp") {
-    return `kn/kp/${slug}`;
-  } else if (book === "dhp") {
-    return `kn/dhp/${slug}`;
-  } else if (book === "ud") {
-    return `kn/ud/vagga${firstNum}/${slug}`;
-  } else if (book === "iti") {
-    return `kn/iti/vagga${findItiVagga(firstNum)}/${slug}`;
-  } else if (book === "snp") {
-    return `kn/snp/vagga${firstNum}/${slug}`;
-  } else if (book === "thag" || book === "thig") {
-    return `kn/${book}/${slug}`;
-  }
-}
-
-function findItiVagga(suttaNumber) {
-  if (suttaNumber >= 1 && suttaNumber <= 10) {
-    return "1";
-  } else if (suttaNumber >= 1 && suttaNumber <= 10) {
-    return "1";
-  } else if (suttaNumber >= 11 && suttaNumber <= 20) {
-    return "2";
-  } else if (suttaNumber >= 21 && suttaNumber <= 27) {
-    return "3";
-  } else if (suttaNumber >= 28 && suttaNumber <= 37) {
-    return "4";
-  } else if (suttaNumber >= 38 && suttaNumber <= 49) {
-    return "5";
-  } else if (suttaNumber >= 50 && suttaNumber <= 59) {
-    return "6";
-  } else if (suttaNumber >= 60 && suttaNumber <= 69) {
-    return "7";
-  } else if (suttaNumber >= 70 && suttaNumber <= 79) {
-    return "8";
-  } else if (suttaNumber >= 80 && suttaNumber <= 89) {
-    return "9";
-  } else if (suttaNumber >= 90 && suttaNumber <= 99) {
-    return "10";
-  } else if (suttaNumber >= 100 && suttaNumber <= 112) {
-    return "11";
-  }
-}
-
-const form = document.getElementById("form");
-const citation = document.getElementById("citation");
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  buildSutta(citation.value);
-  document.location.search = "?" + citation.value;
-});
-
-citation.value = document.location.search.replace("?", "");
