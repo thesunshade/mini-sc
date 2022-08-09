dd05a17b5137a9d33dab2766b5c30abfef161b84
TheSunshade <82448383+thesunshade@users.noreply.github.com>
2022-08-09 09:00:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const bodyTag = document.querySelector("body");
 const previous = document.getElementById("previous");
 const next = document.getElementById("next");
 
+document.onkeyup = function (e) {
+  console.log(e);
+  if (e.altKey && e.key == "q") {
+    alert("q key was pressed");
+    const bodyElement = document.querySelector("body");
+    console.log(bodyElement);
+    bodyElement.style.background = "blue";
+  }
+};
+
 homeButton.addEventListener("click", () => {
   document.location.search = "";
 });
